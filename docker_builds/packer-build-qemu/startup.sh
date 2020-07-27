0135213ba9eb032b6c9252dfe778074fde3e4439
enros <enros@localhost.localdomain>
2020-07-27 01:10:37 +0100

    updated Dockerfile

---
+++ docker_builds/packer-build-qemu/startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+[ -n "$DEBUG" ] && set -x
+
+# Create the kvm node (required --privileged)
+if [ ! -e /dev/kvm ]; then
+  set +e
+  mknod /dev/kvm c 10 $(grep '\<kvm\>' /proc/misc | cut -f 1 -d' ')
+  set -e
+fi
+
+# Run packer
+if [ ! -f /usr/local/bin/packer ]; then
+  /usr/local/bin/packer
+fi
